bd6fbf949d76b5e469d905c3066b363f5520e380
rumbleFTW <007rajdeepghosh@gmail.com>
2022-04-30 22:53:35 +0530

    Added API

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+import json
+import pandas as pd
+
+
+from scrape import scrape
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return json.dumps({'name': 'rajdeep', 'desc': 'API is working'})
+
+@app.route('/historical/daily')
+def getDailyData():
+    scr = scrape()
+    scr.getDaily()
+    dataHist = pd.read_csv('./sih-2022/data/daily.csv')
+    hist = {}
+    for i in range(len(dataHist)):
+        hist[int(dataHist['Day'][i])] = dataHist['Price'][i]
+    return json.dumps(hist)
+
+@app.route('/historical/weekly')
+def getWeeklyData():
+    scr = scrape()
+    scr.getWeekly()
+    dataHist = pd.read_csv('./sih-2022/data/weekly.csv')
+    hist = {}
+    for i in range(len(dataHist)):
+        hist[int(dataHist['Day'][i])] = dataHist['Price'][i]
+    return json.dumps(hist)
+
+@app.route('/historical/monthly')
+def getMonthlyData():
+    scr = scrape()
+    scr.getMonthly()
+    dataHist = pd.read_csv('./sih-2022/data/monthly.csv')
+    hist = {}
+    for i in range(len(dataHist)):
+        hist[int(dataHist['Day'][i])] = dataHist['Price'][i]
+    return json.dumps(hist)
+
+
+app.run()
--- scrape.py
+++ scrape.py
@@ -1,39 +1,41 @@
-from numpy import index_exp
 import requests
 import pandas as pd
-import datetime as dt
 
 key = "LLlFxfWWzAsqU8YRvLvoKQoGwqWOUAfmrITv8TFE"
+class scrape:
 
-dataMonthly = requests.get(f"https://api.eia.gov/series/?series_id=NG.RNGWHHD.W&api_key={key}")
-rawResponseMonthly = dataMonthly.json()['series']
-rawDataMonthly = rawResponseMonthly[0]['data'][::-1]
+    def getMonthly(self):
+        dataMonthly = requests.get(f"https://api.eia.gov/series/?series_id=NG.RNGWHHD.M&api_key={key}")
+        rawResponseMonthly = dataMonthly.json()['series']
+        rawDataMonthly = rawResponseMonthly[0]['data'][::-1]
 
-dataframeMonthly = pd.DataFrame(rawDataMonthly, columns = ['Day', 'Price'])
-nan_value = float("NaN")
-dataframeMonthly.replace("", nan_value, inplace=True)
-dataframeMonthly.dropna(subset = ["Price"], inplace=True)
-dataframeMonthly.to_csv('C:\\Users\\007ra\\OneDrive\\Documents\\Codes\\SiH\\monthly.csv', index = False)
-print('Monthly Data saved!')
+        dataframeMonthly = pd.DataFrame(rawDataMonthly, columns = ['Day', 'Price'])
+        nan_value = float("NaN")
+        dataframeMonthly.replace("", nan_value, inplace=True)
+        dataframeMonthly.dropna(subset = ["Price"], inplace=True)
+        dataframeMonthly.to_csv('./sih-2022/data/monthly.csv', index = False)
+        print('Monthly Data saved!')
 
-dataWeekly = requests.get(f"https://api.eia.gov/series/?series_id=NG.RNGWHHD.W&api_key={key}")
-rawResponseWeekly = dataWeekly.json()['series']
-rawDataWeekly = rawResponseWeekly[0]['data'][::-1]
+    def getWeekly(self):
+        dataWeekly = requests.get(f"https://api.eia.gov/series/?series_id=NG.RNGWHHD.W&api_key={key}")
+        rawResponseWeekly = dataWeekly.json()['series']
+        rawDataWeekly = rawResponseWeekly[0]['data'][::-1]
 
-dataframeWeekly = pd.DataFrame(rawDataWeekly, columns = ['Day', 'Price'])
-nan_value = float("NaN")
-dataframeWeekly.replace("", nan_value, inplace=True)
-dataframeWeekly.dropna(subset = ["Price"], inplace=True)
-dataframeWeekly.to_csv('C:\\Users\\007ra\\OneDrive\\Documents\\Codes\\SiH\\weekly.csv', index = False)
-print('Weekly Data saved!')
+        dataframeWeekly = pd.DataFrame(rawDataWeekly, columns = ['Day', 'Price'])
+        nan_value = float("NaN")
+        dataframeWeekly.replace("", nan_value, inplace=True)
+        dataframeWeekly.dropna(subset = ["Price"], inplace=True)
+        dataframeWeekly.to_csv('./sih-2022/data/weekly.csv', index = False)
+        print('Weekly Data saved!')
+    
+    def getDaily(self):
+        dataDaily = requests.get(f"https://api.eia.gov/series/?series_id=NG.RNGWHHD.D&api_key={key}")
+        rawResponseDaily = dataDaily.json()['series']
+        rawDataDaily = rawResponseDaily[0]['data'][::-1]
 
-dataDaily = requests.get(f"https://api.eia.gov/series/?series_id=NG.RNGWHHD.D&api_key={key}")
-rawResponseDaily = dataDaily.json()['series']
-rawDataDaily = rawResponseDaily[0]['data'][::-1]
-
-dataframeDaily = pd.DataFrame(rawDataDaily, columns = ['Day', 'Price'])
-nan_value = float("NaN")
-dataframeDaily.replace("", nan_value, inplace=True)
-dataframeDaily.dropna(subset = ["Price"], inplace=True)
-dataframeDaily.to_csv('C:\\Users\\007ra\\OneDrive\\Documents\\Codes\\SiH\\daily.csv', index = False)
-print('Daily Data saved!')
+        dataframeDaily = pd.DataFrame(rawDataDaily, columns = ['Day', 'Price'])
+        nan_value = float("NaN")
+        dataframeDaily.replace("", nan_value, inplace=True)
+        dataframeDaily.dropna(subset = ["Price"], inplace=True)
+        dataframeDaily.to_csv('./sih-2022/data/daily.csv', index = False)
+        print('Daily Data saved!')
